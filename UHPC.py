f4bfdaefac73f5ea2b10a424eaee8dc760f4d236
SeifSultan03 <99747202+SeifSultan03@users.noreply.github.com>
2024-10-04 19:00:03 -0500

    update requirements.txt

--- UHPC.py
+++ UHPC.py
@@ -3,8 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import io
-import pandas as pd
-import numpy as np
 from scipy.optimize import minimize
 
 # Full path to your Excel file
